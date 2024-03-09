51f76043c194fce33f711240fe691fbd9e4f9130
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2024-03-09 19:03:13 +0200

    Add files via upload

---
+++ 01-Basic-Syntax-Conditional-Statements-And-Loops-Lab/01-multiplyNumberBy2.js
@@ -0,0 +1,8 @@
+//Write a function that receives a number and prints as result that number multiplied by two.
+
+function multiplyNumBy2(num) {
+    console.log(num*2);
+    }
+multiplyNumBy2(2);
+multiplyNumBy2(5);
+multiplyNumBy2(20);
