74213f5adcd3402fc88d7b8c8d96868c8de83df1
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2024-03-09 19:05:18 +0200

    Delete 01-multiplyNumberBy2.js

--- 01-multiplyNumberBy2.js
+++
@@ -1,8 +0,0 @@
-//Write a function that receives a number and prints as result that number multiplied by two.
-
-function multiplyNumBy2(num) {
-    console.log(num*2);
-    }
-multiplyNumBy2(2);
-multiplyNumBy2(5);
-multiplyNumBy2(20);
